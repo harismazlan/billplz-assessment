ff2a5f87e2cc8cefa965a3eb7aad2813efc4ebac
Haris Mazlan <haris_mazlan@yahoo.co.uk>
2024-05-17 11:41:56 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
 
     <footer class="footer mt-auto py-3 bg-light">
         <div class="container">
-            <span class="text-muted">© 2021 BillPlz - Assessment</span>
+            <span class="text-muted">© 2024 BillPlz - Assessment</span>
         </div>
     </footer>
 
